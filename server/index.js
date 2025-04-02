9dad11123f7b144dbada8518c254c09daeb105a3
Prashant Rai <prashantrai0628@gmail.com>
2025-04-03 01:22:34 +0530

    index.js created

---
+++ server/index.js
@@ -0,0 +1,13 @@
+//   Y50Cbt992dbme8bh        mongodb lms
+//  mongodb+srv://studyprash:Y50Cbt992dbme8bh@cluster0.gpngcki.mongodb.net/
+
+// install express mongoose cors dotenv bcryptjs jsonwebtoken nodemona
+import express from "express";
+
+const app = express();
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
+})
